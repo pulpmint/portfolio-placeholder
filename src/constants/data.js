fb7767f76c7249abdabed03cfd66589c87ce60b6
Manmohan Singh <68352467+pulpmint@users.noreply.github.com>
2025-07-13 22:58:26 +0530

    Added data

---
+++ src/constants/data.js
@@ -0,0 +1,131 @@
+export const DOMAIN = "manmohansingh.dev";
+
+export const MAIL = `hi@${DOMAIN}`;
+export const MAIL_TO = `mailto:${MAIL}`;
+
+export const USERNAME = "pulpmint";
+
+export const NAME = "Manmohan Singh";
+export const TAGLINE = "Building things for the Web";
+
+export const TITLE = `${NAME} - ${TAGLINE}`;
+export const DESC = `A web developer based in India. Working with JavaScript & Figma. ${TAGLINE}.`;
+
+export const GREETINGS = `Hey there! I am ${NAME}.`;
+
+export const PIXEL_PERFECT =
+  "Crafting pixel-perfect, secure & performant web experiences with great attention to detail.";
+
+export const DANPEN = {
+  LABEL: "Danpen",
+  LINK: "https://www.danpen.wtf",
+  YEAR: 2022
+};
+
+const TWITTER_GEMS = {
+  LABEL: "Twitter Gems",
+  LINK: "https://twittrgems.careerleap.io",
+  YEAR: 2021
+};
+
+const COFFEE_HOURS = {
+  LABEL: "Coffee Hours",
+  LINK: "https://www.coffeehours.club",
+  YEAR: 2020
+};
+
+const RIVI = {
+  LABEL: "Rivi",
+  LINK: "https://www.rivi.co",
+  YEAR: 2020
+};
+
+export const PROJECTS = [DANPEN, TWITTER_GEMS, COFFEE_HOURS, RIVI];
+
+export const EXPERIENCES = [
+  {
+    DESG: "Frontend",
+    LABEL: "PayU",
+    YEAR: "2023"
+  },
+  {
+    DESG: "Full Stack",
+    LABEL: "Standard Chartered Bank",
+    YEAR: "2020"
+  },
+  {
+    DESG: "Frontend (Intern)",
+    LABEL: "Rivi",
+    YEAR: "2019"
+  }
+];
+
+export const SKILLS = [
+  "React.js",
+  "Next.js",
+  "Vue.js",
+  "Nuxt.js",
+  "Node.js",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "Figma"
+];
+
+export const AWARDS = [
+  {
+    ...TWITTER_GEMS,
+    AWARD: "Product Hunt - #1 POD",
+    LINK: "https://www.producthunt.com/products/twittr-gems"
+  }
+];
+
+export const GITHUB = {
+  LABEL: "GitHub",
+  LINK: "https://www.github.com/pulpmint"
+};
+
+export const CODEPEN = {
+  LABEL: "CodePen",
+  LINK: "https://www.codepen.io/pulpmint"
+};
+
+export const TWITTER = {
+  LABEL: "Twitter",
+  LINK: "https://www.twitter.com/pulpmint"
+};
+
+export const HASHNODE = {
+  LABEL: "Hashnode",
+  LINK: "https://pulpmint.hashnode.dev",
+  OPTIONAL: "My Blog"
+};
+
+export const SOCIALS = [GITHUB, CODEPEN, TWITTER, HASHNODE];
+
+export const FAQS = [
+  {
+    QUESTION: "What's with all the text?",
+    ANSWER:
+      "I'm busy working on the actual site. It is expected to go live within the next 69 years."
+  },
+  {
+    QUESTION: "What did you use for this site?",
+    ANSWER:
+      "For this one, I just used Astro & some good old CSS for styles. Plain & simple."
+  },
+  {
+    QUESTION: "What are you using for the actual site?",
+    ANSWER:
+      "I'm using Next.js & TypeScript along with TailwindCSS for styles & GSAP for animations."
+  },
+  {
+    QUESTION: "This is ugly.",
+    ANSWER: "Yes but that is not a question."
+  }
+];
+
+export const MORE_QUESTIONS = "Have more questions?";
+
+export const COPYRIGHT = `🧙‍♂️ Manmohan Singh`;
